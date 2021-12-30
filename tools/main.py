16b7aab26d7c070ab867c43464b4a5f929fadef9
Angelo Delicato <angelo.delicato@secsi.io>
2021-12-30 11:01:34 +0100

    Change tool structure and add sublist3r

--- tools/main.py
+++ tools/main.py
@@ -1,9 +1,46 @@
-from tools.dirb import config as dirb
-from tools.dirhunt import config as dirhunt
-from tools.ffuf import config as ffuf
-from tools.gobuster import config as gobusterdns
+import helper
 
-tools = [dirb, dirhunt, ffuf, gobusterdns]
+organization = 'secsi'
+
+tools = {
+    'dirb': {
+        'name': organization+'/dirb',
+        'version': 'latest',
+        'buildargs': {
+            'LAST_UBUNTU_VERSION': helper.get_latest_docker_hub_version('ubuntu')
+        }
+    },
+    'dirhunt': {
+        'name': organization+'/dirhunt',
+        'version': helper.get_latest_pip_version('dirhunt'),
+        'buildargs': {
+            'DIRHUNT_VERSION': helper.get_latest_pip_version('dirhunt')
+        }
+    },
+    'ffuf': {
+        'name': organization+'/ffuf',
+        'version': helper.get_latest_github_release('ffuf/ffuf', target_string='linux_amd64')['version'][1:], # Remove the leading 'v'
+        'buildargs': {
+            'LAST_UBUNTU_VERSION': helper.get_latest_docker_hub_version('ubuntu'),
+            'FFUF_DOWNLOAD_URL': helper.get_latest_github_release('ffuf/ffuf', target_string='linux_amd64')['url']
+        }
+    },
+    'gobuster': {
+        'name': organization+'/gobuster',
+        'version': helper.get_latest_github_release('OJ/gobuster', target_string='linux-amd64')['version'][1:], # Remove the leading 'v',
+        'buildargs': {
+            'LAST_UBUNTU_VERSION': helper.get_latest_docker_hub_version('ubuntu'),
+            'GOBUSTER_DOWNLOAD_URL': helper.get_latest_github_release('OJ/gobuster', target_string='linux-amd64')['url']
+        }
+    },
+    'sublist3r': {
+        'name': organization+'/sublist3r',
+        'version': helper.get_latest_github_release_no_browser_download('aboul3la/Sublist3r')['version'],
+        'buildargs': {
+            'SUBLIST3R_DOWNLOAD_URL': helper.get_latest_github_release_no_browser_download('aboul3la/Sublist3r')['url']
+        }
+    }
+}
 
 # List of all imported tools 
 def get_tools():
@@ -11,5 +48,5 @@ def get_tools():
 
 # Get a single tool for specific build
 def get_single_tool(tool_name):
-    tool = next((t for t in tools if t.__name__.split('.')[1] == tool_name), None) # returns None if tool is not found
+    tool = next((tools[t] for t in tools if t == tool_name), None) # returns None if tool is not found
     return tool 
