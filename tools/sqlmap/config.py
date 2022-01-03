4d24df7e30e67d738f920ca92e3c33448473cad7
Angelo Delicato <angelo.delicato@secsi.io>
2022-01-03 14:06:50 +0100

    Add 'sqlmap'

---
+++ tools/sqlmap/config.py
@@ -0,0 +1,16 @@
+import helper
+
+def get_config(organization, common_args):
+    api_results = {
+        'SQLMAP_GITHUB_INFO': helper.get_latest_github_release_no_browser_download('sqlmapproject/sqlmap'),
+    }
+    
+    config = {
+        'name': organization+'/sqlmap',
+        'version': api_results['SQLMAP_GITHUB_INFO']['version'],
+        'buildargs': {
+            'PYTHON_ALPINE_VERSION': common_args['PYTHON_ALPINE_VERSION'],
+            'SQLMAP_DOWNLOAD_URL': api_results['SQLMAP_GITHUB_INFO']['url']
+        }
+    }
+    return config
