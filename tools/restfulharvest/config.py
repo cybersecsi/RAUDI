5d7afc85ecd2976019d5bed0916d90ad7d5f7725
Angelo Delicato <angelo.delicato@secsi.io>
2022-01-04 09:54:31 +0100

    Add 'theHarvester' both as normal tool and REST

---
+++ tools/restfulharvest/config.py
@@ -0,0 +1,16 @@
+import helper
+
+def get_config(organization, common_args):
+    api_results = {
+        'THEHARVESTER_GITHUB_INFO': helper.get_latest_github_release_no_browser_download('laramies/theHarvester'),
+    }
+    
+    config = {
+        'name': organization+'/restfulharvest',
+        'version': api_results['THEHARVESTER_GITHUB_INFO']['version'],
+        'buildargs': {
+            'PYTHON_ALPINE_VERSION': common_args['PYTHON_ALPINE_VERSION'],
+            'THEHARVESTER_DOWNLOAD_URL': api_results['THEHARVESTER_GITHUB_INFO']['url']
+        }
+    }
+    return config
