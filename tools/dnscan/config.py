c37535a96218ee2967534173897b53c5ed32a72f
Daniele Capone <daniele.capone@secsi.io>
2022-01-10 16:29:15 +0100

    Add get_latest_github_commit fn in helper.py; add dnscan

---
+++ tools/dnscan/config.py
@@ -0,0 +1,17 @@
+import helper
+
+def get_config(organization, common_args):
+    api_results = {
+        'DNSCAN_GITHUB_INFO': helper.get_latest_github_commit('rbsec/dnscan'),
+    }
+    
+    config = {
+        'name': organization+'/dnscan',
+        'version': api_results['DNSCAN_GITHUB_INFO'],
+        'buildargs': {
+            'PYTHON_ALPINE_VERSION': common_args['PYTHON_ALPINE_VERSION'],
+            'DNSCAN_DOWNLOAD_URL': "https://github.com/rbsec/dnscan"
+        },
+        'tests': ['-h']
+    }
+    return config
