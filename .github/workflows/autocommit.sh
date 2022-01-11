823689ca0552ed753499f0b51f2fb1c7a4cb52ad
Angelo Delicato <angelo.delicato@secsi.io>
2022-01-11 09:31:58 +0100

    Switch to Markdown for auto-commit logs

--- .github/workflows/autocommit.sh
+++ .github/workflows/autocommit.sh
@@ -3,6 +3,6 @@ echo "Printing workflow log..."
 cat /tmp/log.txt
 grep -B3 "Image successfully pushed" /tmp/log.txt | grep "secsi/" | awk '{print $6}' >> /tmp/updated_images.txt
 if [ -s /tmp/updated_images.txt ] ; then 
-    printf "\n["$(date +%F)"]\n" >> $PWD/LOG.txt ;
-    for i in `cat /tmp/updated_images.txt`; do split=(${i//:/ }) ; echo "${split} updated to version ${split[1]}" >> $PWD/LOG.txt ; done
+    printf "\n### ["$(date +%F)"]\n" >> $PWD/LOG.txt ;
+    for i in `cat /tmp/updated_images.txt`; do split=(${i//:/ }) ; echo "- ${split} updated to version ${split[1]}" >> $PWD/LOG.md ; done
 fi
---
+++ LOG.md
@@ -0,0 +1,5 @@
+# RAUDI Automatic Update Logs
+
+### [2022-01-10]
+- secsi/dnscan updated to version 20220107
+- secsi/massdns updated to version 1.0.0
