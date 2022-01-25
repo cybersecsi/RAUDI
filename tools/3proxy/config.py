dfc32a467af66901c89d405cefefbcb696e4ecdb
Angelo Delicato <angelo.delicato@secsi.io>
2022-01-25 09:38:09 +0100

    Update 3proxy

--- tools/3proxy/config.py
+++ tools/3proxy/config.py
@@ -5,19 +5,15 @@ DEFAULT_DIRNAME = os.path.basename(os.path.dirname(__file__))
 
 def get_config(organization, common_args):
     api_results = {
-        # See the README for a detailed documentation of those helpers
-        #'GITHUB_INFO': helper.get_latest_github_release('<repo_name>', '<target_string>'),
-        'GITHUB_INFO': helper.get_latest_github_release_no_browser_download('3proxy/3proxy'),
-        #'GITHUB_INFO': helper.get_latest_github_tag_no_browser_download('<repo_name>'),
+        'GITHUB_INFO': helper.get_latest_github_release('3proxy/3proxy', target_string="x86_64.deb"),
     }
-    print(api_results['GITHUB_INFO']['url'])
+
     config = {
         'name': organization+'/'+DEFAULT_DIRNAME,
         'version' : api_results['GITHUB_INFO']['version'],
         'buildargs': {
             'DEBIAN_SLIM_VERSION': common_args['DEBIAN_SLIM_VERSION'],
-            'DOWNLOAD_URL': helper.get_deb_from_github('3proxy', '3proxy', api_results['GITHUB_INFO']['version'])
-            # 'DOWNLOAD_URL': api_results['GITHUB_INFO']['url'],
+            'DOWNLOAD_URL': api_results['GITHUB_INFO']['url']
         },
         'tests': ['--help']
     }
