c8f8c9073cb29c9d74f461cc49756be48977c63e
thelicato <thelicato@users.noreply.github.com>
2022-05-27 09:49:35 +0200

    Add exit code in autocommit.sh

--- .github/workflows/autocommit.sh
+++ .github/workflows/autocommit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "Executing autocommit script"
 grep "successfully pushed to Docker Hub" /tmp/log.txt | awk '{print $2}' >> /tmp/updated_images.txt
 if [ -s /tmp/updated_images.txt ] ; then 
     printf "\n### ["$(date +%F)"]\n" >> $PWD/LOG.md ;
     for i in `cat /tmp/updated_images.txt`; do split=(${i//:/ }) ; echo "- ${split} updated to version ${split[1]}" >> $PWD/LOG.md ; done
 fi
+
+# Use the exit code got from the log
+exit_code=$(grep "RAUDI completed" /tmp/log.txt | awk '{print $8}')
+exit $exit_code
