cd2028293458d42ede363aa0a6a59b1e91023613
thelicato <thelicato@users.noreply.github.com>
2022-10-13 09:54:34 +0200

    Fix gobuster tool and switch to normal golang install

--- tools/gobuster/config.py
+++ tools/gobuster/config.py
@@ -9,9 +9,8 @@ def get_config(organization, common_args):
         'name': organization+'/gobuster',
         'version': helper.clean_version(api_results['GOBUSTER_GITHUB_INFO']['version']),
         'buildargs': {
-            'LATEST_ALPINE_VERSION': common_args['LATEST_ALPINE_VERSION'],
-            'GOBUSTER_DOWNLOAD_URL': api_results['GOBUSTER_GITHUB_INFO']['url']
+            'GOLANG_ALPINE_VERSION': common_args['GOLANG_ALPINE_VERSION'],
         },
-        'tests': ['--help']
+        'tests': ['-h']
     }
     return config
