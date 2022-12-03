31179076b038af9249a401814b577fafa8a6284b
thelicato <thelicato@users.noreply.github.com>
2022-12-03 20:08:10 +0100

    Update 3proxy to support arm6

--- tools/3proxy/config.py
+++ tools/3proxy/config.py
@@ -5,7 +5,7 @@ DEFAULT_DIRNAME = os.path.basename(os.path.dirname(__file__))
 
 def get_config(organization, common_args):
     api_results = {
-        'GITHUB_INFO': helper.get_latest_github_release('3proxy/3proxy', target_string="x86_64.deb"),
+        'GITHUB_INFO': helper.get_latest_github_release_no_browser_download('3proxy/3proxy'),
     }
 
     config = {
@@ -13,7 +13,7 @@ def get_config(organization, common_args):
         'version' : helper.clean_version(api_results['GITHUB_INFO']['version']),
         'buildargs': {
             'DEBIAN_SLIM_VERSION': common_args['DEBIAN_SLIM_VERSION'],
-            'DOWNLOAD_URL': api_results['GITHUB_INFO']['url']
+            'DOWNLOAD_URL': api_results['GITHUB_INFO']['url'],
         },
         'tests': []
     }
