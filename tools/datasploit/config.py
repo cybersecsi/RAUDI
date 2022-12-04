97e0c468ffd8685bc5b6176361f256f43f040298
thelicato <thelicato@users.noreply.github.com>
2022-12-04 16:33:08 +0100

    Update datasploit Dockerfile

--- tools/datasploit/config.py
+++ tools/datasploit/config.py
@@ -3,7 +3,7 @@ import helper
 
 def get_config(organization, common_args):
     api_results = {
-        'DATASPLOIT_GITHUB_INFO': helper.get_latest_github_commit('DataSploit/datasploit'),
+        'DATASPLOIT_GITHUB_INFO': helper.get_latest_github_commit('dvopsway/datasploit'),
     }
 
     config = {
@@ -11,7 +11,7 @@ def get_config(organization, common_args):
         'version': helper.clean_version(api_results['DATASPLOIT_GITHUB_INFO']),
         'buildargs': {
             'PYTHON2_ALPINE_VERSION': common_args['PYTHON2_ALPINE_VERSION'],
-            'DATASPLOIT_DOWNLOAD_URL':  "https://github.com/DataSploit/datasploit"
+            'DATASPLOIT_DOWNLOAD_URL':  "https://github.com/dvopsway/datasploit"
         },
         'tests': ['-h']
     }
