5bb5944e8d0950dbd42770febc897eb6523924b5
thelicato <thelicato@users.noreply.github.com>
2024-03-04 11:17:35 +0100

    fix: update knockpy

--- tools/knockpy/config.py
+++ tools/knockpy/config.py
@@ -1,16 +1,17 @@
 import helper
 
+
 def get_config(organization, common_args):
     api_results = {
         'KNOCKPY_GITHUB_INFO': helper.get_latest_github_release_no_browser_download('guelfoweb/knock'),
     }
-    
+
     config = {
         'name': organization+'/knockpy',
         'version': helper.clean_version(api_results['KNOCKPY_GITHUB_INFO']['version']),
         'buildargs': {
             'PYTHON_ALPINE_VERSION': common_args['PYTHON_ALPINE_VERSION'],
-            'KNOCKPY_DOWNLOAD_URL': api_results['KNOCKPY_GITHUB_INFO']['url']
+            'KNOCKPY_DOWNLOAD_URL': "https://github.com/guelfoweb/knock"
         },
         'tests': ['-h']
     }
