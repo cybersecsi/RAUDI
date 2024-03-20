f3d793bfc154b80fb69ca9ebca95d34c535161b1
Daniele <daniele-capone@users.noreply.github.com>
2024-03-20 18:02:55 +0100

    fix: update theharvester

--- tools/theharvester/config.py
+++ tools/theharvester/config.py
@@ -9,7 +9,6 @@ def get_config(organization, common_args):
         'name': organization+'/theharvester',
         'version': helper.clean_version(api_results['THEHARVESTER_GITHUB_INFO']['version']),
         'buildargs': {
-            'PYTHON_ALPINE_VERSION': common_args['PYTHON_ALPINE_VERSION'],
             'THEHARVESTER_DOWNLOAD_URL': api_results['THEHARVESTER_GITHUB_INFO']['url']
         },
         'tests': ['-h']
