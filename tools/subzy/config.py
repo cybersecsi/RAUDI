3d4a2c9227d7c9b25b627e4612207e65cebb9cdf
Daniele <daniele-capone@users.noreply.github.com>
2024-09-11 09:24:41 +0200

    fix: update subzy

--- tools/subzy/config.py
+++ tools/subzy/config.py
@@ -2,15 +2,16 @@ import helper
 
 def get_config(organization, common_args):
     api_results = {
-        'GITHUB_INFO': helper.get_latest_github_commit('LukaSikic/subzy')
+        'GITHUB_INFO': helper.get_latest_github_tag_no_browser_download('PentestPad/subzy')
     }
 
     config = {
         'name': organization+'/subzy',
-        'version': helper.clean_version(api_results['GITHUB_INFO']),
+        'version': helper.clean_version(api_results['GITHUB_INFO']['version']),
         'buildargs': {
             'GOLANG_ALPINE_VERSION': common_args['GOLANG_ALPINE_VERSION'],
             'LATEST_ALPINE_VERSION': common_args['LATEST_ALPINE_VERSION'],
+            'DOWNLOAD_URL': api_results['GITHUB_INFO']['url']
         },
         'tests': ['--help']
     }
